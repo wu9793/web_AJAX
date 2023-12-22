677055731d95cf9f6e45aa4573ee0805bed51d5c
wu <timmy61501@gmail.com>
2023-12-22 16:39:45 +0800

    代入資料表school

--- app2/api/query.php
+++ app2/api/query.php
@@ -1,5 +1,17 @@
 <?php
+include_once "db.php";
 //處理查詢資料的請求
-
+switch($_GET['do']){
+    case "all";
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($Student->all());
+    break;
+    case 'sex':
+        dd($_GET);
+    break;
+    case 'class':
+    
+    break;
+}
 
 ?>
